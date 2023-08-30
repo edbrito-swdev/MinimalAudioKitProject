a3f43555098e8cd4a45153482d48088f2fdc36fa
Eduardo Brito <edbrito.swdev@gmail.com>
2023-08-31 00:36:06 +0100

    Initial Commit

---
+++ MinimalAudioKitProject/MinimalAudioKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalAudioKitProjectApp.swift
+//  MinimalAudioKitProject
+//
+//  Created by Eduardo Brito on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MinimalAudioKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
