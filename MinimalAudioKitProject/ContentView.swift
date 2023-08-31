df41f6d9ed4fa05e9897b52187a817335f766bbc
Eduardo Brito <edbrito.swdev@gmail.com>
2023-08-31 02:21:59 +0100

    Missed file in previous commit.

--- MinimalAudioKitProject/ContentView.swift
+++ MinimalAudioKitProject/ContentView.swift
@@ -6,21 +6,63 @@
 //
 
 import SwiftUI
+import AudioKit
+import SoundpipeAudioKit
+
+class ThreadedPlayer: Thread {
+    
+    var osc: MorphingOscillator;
+    
+    init(mainOsc: MorphingOscillator) {
+        osc = mainOsc
+    }
+    
+    override func main() {
+        osc.play()
+        while true {
+            osc.frequency = Float.random(in: 200 ... 800)
+            osc.amplitude = Float.random(in: 0.0 ... 0.3)
+            usleep(100_000)
+        }
+    }
+}
+
+class ToneGenerator: ObservableObject {
+    var osc = MorphingOscillator(index:2.75)
+    let engine = AudioEngine()
+    var player: ThreadedPlayer;
+    
+    @Published var playing: Bool = false {
+        didSet {
+            if playing {
+                player = ThreadedPlayer(mainOsc: osc)
+                player.start()
+            } else {
+                osc.stop()
+                player.cancel()
+            }
+        }
+    }
+    
+    init() {
+        engine.output = osc
+        try! engine.start()
+        player = ThreadedPlayer(mainOsc: osc)
+    }
+    
+    func toggle() {
+        playing = !playing
+    }
+}
 
 struct ContentView: View {
+    let generator = ToneGenerator()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Simple Oscilator") {
+            generator.toggle()
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
